fd704837ea675a90a6420f99c296c5c1db621a96
RebecaTI <mebuffbeka@gmail.com>
2024-08-21 16:11:27 -0300

    origamid modules

---
+++ JavaScript ES6/04-modules-introducao/js/script.js
@@ -0,0 +1,12 @@
+import initScrollSuave from './modules/scroll-suave.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import init from './modules/tabnav.js';
+import teste from './modules/teste.js';
+
+teste();
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+init();
