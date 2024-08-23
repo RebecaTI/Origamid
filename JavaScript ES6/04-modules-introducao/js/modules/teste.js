f21da36348b2ce70b367c52457670d52b8a202be
RebecaTI <mebuffbeka@gmail.com>
2024-08-23 10:37:41 -0300

    modules, limpando arquivos p dar continuidade ao estudo sobre modules

--- JavaScript ES6/04-modules-introducao/js/modules/teste.js
+++ JavaScript ES6/04-modules-introducao/js/modules/teste.js
@@ -1,3 +1,10 @@
-export default function testando(){
+export function testando() {
   console.log('Isso é teste')
 }
+export function teste2() {
+  console.log('Isso é o teste 2')
+}
+
+const esseNome = 'Andre';
+
+export const senha = 7876543;
