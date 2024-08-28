c82160284a23566919e9f44706f6612a12aa22c4
RebecaTI <mebuffbeka@gmail.com>
2024-08-28 09:32:28 -0300

    adicionando dropdown

--- JavaScript ES6/07- Dropdown/js/modules/dropdown-menu.js
+++ JavaScript ES6/07- Dropdown/js/modules/dropdown-menu.js
@@ -1,3 +1,35 @@
 export default function initDropdownMenu() {
 
 }
+
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropdownMenus.forEach(menu => {
+  ['touchstart', 'click'].forEach(userEvent => {
+    menu.addEventListener(userEvent, handleClick);
+  })
+})
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.add('active');
+  outsideClick(this, () => {
+    this.classList.remove('active');
+  });
+}
+
+function outsideClick(element, callback) {
+  const html = document.documentElement;
+  const outside = 'data-outside';
+
+  html.addEventListener('click', handleOutsideClick);
+  element.setAttribute(outside, '');
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      html.removeEventListener('click', handleOutsideClick);
+      callback();
+    }
+  }
+}
+
+// Fazendo o bubble
