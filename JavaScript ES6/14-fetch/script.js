b6d451ef06ebb2670a6cf4b22f41acd772923bfa
RebecaTI <mebuffbeka@gmail.com>
2024-09-04 08:24:44 -0300

    aula sobre fetch e apis

--- JavaScript ES6/14-fetch/script.js
+++ JavaScript ES6/14-fetch/script.js
@@ -1,7 +1,9 @@
 // fetch interna
 //API Significa que é uma intervace de aplicação que conseguimos interagir com ela atravez de codigos.
-// const doc = fetch('./doc.txt');
 
+
+
+// const doc = fetch('./doc.txt');
 // doc
 //   .then(r => r.text())
 //   .then(body => {
@@ -9,8 +11,9 @@
 //     conteudo.innerText = body;
 //   });
 
-// const style = fetch('./style.css');
 
+
+// const style = fetch('./style.css');
 // style
 //   .then(r => r.text())
 //   .then(body => {
@@ -21,15 +24,55 @@
 //     conteudo.appendChild(style);
 //   });
 
-const sobre = fetch('./sobre.html');
 
-const div = document.createElement('div');
 
-sobre
-  .then(r => r.text())
-  .then(body => {
-    div.innerHTML = body
-    const titulo = div.querySelector('h1');
-    document.querySelector('h1').innerText = titulo.innerText;
-    console.log(titulo);
-  });
+// const sobre = fetch('./sobre.html');
+// const div = document.createElement('div');
+// sobre
+//   .then(r => r.text())
+//   .then(body => {
+//     div.innerHTML = body
+//     const titulo = div.querySelector('h1');
+//     document.querySelector('h1').innerText = titulo.innerText;
+//     console.log(titulo);
+//   });
+
+
+
+// const imagem = fetch("./imagem.jpg");
+// imagem
+//   .then(r => r.blob())
+//   .then(body => {
+//     const blobUrl = URL.createObjectURL(body);
+//     const imagemDom = document.querySelector('img');
+//     console.log(imagemDom)
+//     imagemDom.src = blobUrl;
+//   });
+
+
+
+// const cep = fetch("https://viacep.com.br/ws/01001000/json/");
+// cep
+//   .then(r => {
+//     const r2 = r.clone();
+//     r.text().then((text) => {
+//       console.log(text);
+//     });
+//     r2.json().then((json) => {
+//       console.log(json);
+//     });
+//     console.log(r)
+//   })
+//   .then(body => {
+//     console.log(body)
+//   });
+
+
+
+const imagem = fetch('https://www.google.com');
+imagem.then(response => {
+  console.log(response.type);
+  if (response.status === 404) {
+    console.log('Página não existe');
+  }
+});
