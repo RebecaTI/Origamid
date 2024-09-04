183fe87b9585bd1b8ed0eae8418461273e0cf15f
RebecaTI <mebuffbeka@gmail.com>
2024-09-04 11:09:33 -0300

    Adicionando opções do segundo parametro p acesso de API

--- JavaScript ES6/16-APIA-e-HTTP/script.js
+++ JavaScript ES6/16-APIA-e-HTTP/script.js
@@ -1,5 +1,11 @@
-fetch('https://pokeapi.co/api/v2/pokemon/')
-  .then(response => response.json())
-  .then(pokemon => {
-    console.log(pokemon)
-  })
+// fetch('https://pokeapi.co/api/v2/pokemon/')
+//   .then(response => response.json())
+//   .then(pokemon => {
+//     console.log(pokemon)
+//   })
+const url = 'https://jsonplaceholder.typicode.com/users/1/posts';
+const options = {
+  method: 'POST'
+}
+
+fetch(url, options);
