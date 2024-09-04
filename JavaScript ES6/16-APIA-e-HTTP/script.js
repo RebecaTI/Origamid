ff0399a678ffb084f1033553c149f6532dd28543
RebecaTI <mebuffbeka@gmail.com>
2024-09-04 15:10:47 -0300

    Method e cors

--- JavaScript ES6/16-APIA-e-HTTP/script.js
+++ JavaScript ES6/16-APIA-e-HTTP/script.js
@@ -3,23 +3,34 @@
 //   .then(pokemon => {
 //     console.log(pokemon)
 //   })
-const url = 'https://jsonplaceholder.typicode.com/users/1/posts';
-const options = {
-  method: 'POST',
-  body: '{"title": "Javascript"}',
-  headers: {
-    "Content-Type": "application/json; charset=utf-8"
-  }
-}
-//Dentro do body é do tipo JSON
 
-fetch(url, options)
-  .then(response => response.json())
-  .then(json => console.log(json))
 
-  //Methods
-  //GET 
-  //POST 
-  //PUT
-  //DELETE
-  //Link com os metodos https://www.origamid.com/slide/javascript-completo-es6/#/0605-api-e-http/5
+
+// const url = 'https://jsonplaceholder.typicode.com/users/1/posts';
+// const options = {
+//   method: 'POST',
+//   body: '{"title": "Javascript"}',
+//   headers: {
+//     "Content-Type": "application/json; charset=utf-8"
+//   }
+// }
+// //Dentro do body é do tipo JSON
+
+// fetch(url, options)
+//   .then(response => {
+//     console.log(response.headers.forEach(console.log));
+//   })
+
+//Methods
+//GET 
+//POST 
+//PUT
+//DELETE
+//Link com os metodos https://www.origamid.com/slide/javascript-completo-es6/#/0605-api-e-http/5
+
+
+
+const url = 'https://cors-anywhere.herokuapp.com/https://www.google.com/';
+fetch(url)
+  .then(response => response.text())
+  .then(text => console.log(text));
