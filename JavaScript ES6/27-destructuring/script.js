8069b94f5ca081ef86c8446609b3f17dfe7700b8
RebecaTI <mebuffbeka@gmail.com>
2024-09-09 07:50:52 -0300

    usando o destructuring p declarar varias varias com nomes parecidos

--- JavaScript ES6/27-destructuring/script.js
+++ JavaScript ES6/27-destructuring/script.js
@@ -1,8 +1,59 @@
-const carro = {
-  marca: 'Honda',
-  ano: 2018
-}
+// const carro = {
+//   marca: 'Honda',
+//   ano: 2018
+// }
 
-const { marca, ano, portas } = carro;
+// const { marca, ano, portas } = carro;
 
-console.log(marca, ano, portas);
+// console.log(marca, ano, portas);
+
+
+
+// const cliente = {
+//   nome: 'Andre',
+//   compras: {
+//     digitais: {
+//       livros: ['Livro 1', 'Livro 2'],
+//       videos: ['Videos JS', 'Video HTML']
+//     },
+//     fisicas: {
+//       cadernos: ['Caderno 1']
+//     }
+//   }
+// }
+
+// // console.log(cliente.compras.digitais.livros);
+// // console.log(cliente.compras.digitais.videos);
+
+// const { digitais, fisicas, digitais: { livros, videos } } = cliente.compras;
+// console.log(livros);
+// console.log(fisicas);
+
+
+
+// const cliente = {
+//   nome: 'Andre',
+//   compras: 10,
+// }
+
+// const { nome: nomeAlterado, email = 'email@gmail.com' } = cliente;
+// //Eu quero dizer q essa propriedade na verdade utilize outro nome por ex 'nomeAndre'
+
+// console.log(email)
+
+
+
+// const frutas = ['Banana', 'Uva', 'Morango'];
+
+// const [primeira, segunda, terceira] = frutas;
+
+// console.log(frutas[0])
+
+
+
+// Declarando varias variaveis com nomes parecidos
+const primeiro = 'Item 1';
+const segundo = 'Item 2';
+const terceira = 'Item 3';
+
+const [primeiroO, segundoO, terceiraO] = ['Item 1', 'Item 2', 'Item 3'];
