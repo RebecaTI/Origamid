d12240e7ff489c1cf819c547bc65105d11cf9a86
RebecaTI <mebuffbeka@gmail.com>
2024-09-11 09:08:26 -0300

    acessando o header da API

---
+++ JavaScript ES6/30-loops-e-iterable/script.js
@@ -0,0 +1,10 @@
+const frutas = ['Banana', 'Morango', 'Uva'];
+const frase = 'Isso é Javascript';
+fetch('https://pokeapi.co/api/v2/pokemon/');
+
+for (const fruta of frutas) {
+  console.log(fruta)
+}
+
+// .then(({ headers }) => console.log(headers));
+//.then(response => console.log(response));
