22218a3e6e08963a35706651e2953bc615e0650c
RebecaTI <mebuffbeka@gmail.com>
2024-09-11 09:55:39 -0300

    fot..in  e  for ...of

--- JavaScript ES6/30-loops-e-iterable/script.js
+++ JavaScript ES6/30-loops-e-iterable/script.js
@@ -1,10 +1,57 @@
+/*
+for ...of
+Ideal para arrays e strings
+for ...in
+Ideal para objetos
+*/
+
+const buttons = document.querySelectorAll('button');
+
+for (const btn of buttons) {
+  btn.style.color = 'blue';
+}
+// console.log(...buttons)
+
+
 const frutas = ['Banana', 'Morango', 'Uva'];
 const frase = 'Isso é Javascript';
-fetch('https://pokeapi.co/api/v2/pokemon/');
 
-for (const fruta of frutas) {
-  console.log(fruta)
-}
+// for (const fruta of frutas) {
+//   console.log(fruta);
+// }
 
+// for (const char of frase) {
+//   console.log(char);
+// }
+
+
+
+// fetch('https://pokeapi.co/api/v2/pokemon/');
 // .then(({ headers }) => console.log(headers));
 //.then(response => console.log(response));
+
+
+
+const carro = {
+  marca: 'Honda',
+  ano: 2018,
+}
+
+Object.defineProperties(carro, {
+  rodas: {
+    value: 4,
+    enumerable: true,
+  }
+})
+
+for (const f in frutas) {
+  console.log(frutas[f])
+}
+
+
+const btnUnico = document.querySelector('button');
+const btnStyles = getComputedStyle(btnUnico);
+
+for (const style in btnStyles) {
+  console.log(`${style}: ${btnStyles[style]}`);
+}
