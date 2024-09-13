cffefce80cddac684203b47fff3d8b0cf7257ea6
RebecaTI <mebuffbeka@gmail.com>
2024-09-13 10:41:36 -0300

    regex

--- JavaScript ES6/31-Regex/script.js
+++ JavaScript ES6/31-Regex/script.js
@@ -38,10 +38,85 @@
 
 
 
-const frase3 = 'JavaScript, TypeScript, CofeeScript, Java';
-const regexp3 = /\w+/g;
+// const frase3 = 'JavaScript, TypeScript, CofeeScript, Java';
+// const regexp3 = /\w+/g;
 
-let regexpResult;
-while ((regexpResult = regexp3.exec(frase3)) !== null) {
-  console.log(regexpResult[0]);
-}
+// let regexpResult;
+// while ((regexpResult = regexp3.exec(frase3)) !== null) {
+//   console.log(regexpResult[0]);
+// }
+
+
+
+// const frase4 = 'JavaScript, TypeScript, CofeeScript, Java';
+// const regexp4 = /[0-9]+/;
+// const regexp5 = /Script/g;
+
+// // const resultados = frase4.match(regexp4);
+// const resultados = frase4.split(regexp5);
+// //O split vai remover tudo que ele der match e vai criar um novo item ed array apartir dele
+// console.log(resultados);
+
+
+
+
+// const tags = `
+// <ul>
+//   <li>Item 1</li>
+//   <li>Item 2</li>
+// </ul>
+// `;
+
+// const regexp6 = /(?<=<\/?)\w+/g;
+// //o nome é lookBehind
+
+// const regexp7 = /<li/g;
+
+// const resultado2 = tags.replace(regexp7, '$& class="ativa"');
+
+// console.log(resultado2);
+
+
+
+const emails = `
+empresa@email.com
+contato@email.com
+suporte@email.com
+`;
+
+const emailsErrados = `joao@homail.com.br
+marta@ggmail.com.br
+bruna@oulook.com.br
+andre@origamid.com`
+
+const regexp8 = /(\w+@)[\w.]+/g;
+const regexp9 = /(\w+@)([\w.]+)/g;
+
+// const resultado3 = emails.replace(regexp8, `$1gmail.com`);
+const resultado3 = emails.replace(regexp8, function (...args) {
+  // console.log(args);
+  return '$1X';
+});
+// console.log(resultado3);
+
+const resultado4 = emailsErrados.replace(regexp9, function (...args) {
+  // console.log(args[2]);
+  if (args[2] === 'homail.com.br') {
+    return args[1] + 'hotmail.com.br'
+  } else if (args[2] === 'ggmail.com.br') {
+    return args[1] + 'gmail.com.br'
+  } else if (args[2] === 'oulook.com.br') {
+    return args[1] + 'outlook.com.br'
+  } else {
+    return args[0];
+  }
+});
+
+console.log(resultado4)
+
+
+
+
+
+
+
