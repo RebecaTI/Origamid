b01b0ff713b7118382ff5a229ddd70085f82acac
RebecaTI <mebuffbeka@gmail.com>
2024-11-15 16:41:48 -0300

    react arrays

--- React/06-205-Ferrament-FrontEnd/src/index.jsx
+++ React/06-205-Ferrament-FrontEnd/src/index.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx';
-import App2 from './App2.jsx';
+// import App2 from './App2.jsx';
+import App3 from './App3.jsx';
 
 // createRoot(document.getElementById('root')).render(<App />)
-createRoot(document.getElementById('root-exercicio')).render(<App2 />);
+// createRoot(document.getElementById('root-exercicio')).render(<App2 />);
+createRoot(document.getElementById('root-arrays')).render(<App3 />);
