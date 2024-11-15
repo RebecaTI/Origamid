670231dd1320ba26664317c55aa14136183c932a
RebecaTI <mebuffbeka@gmail.com>
2024-11-15 19:16:21 -0300

    react components

--- React/06-205-Ferrament-FrontEnd/src/index.jsx
+++ React/06-205-Ferrament-FrontEnd/src/index.jsx
@@ -2,8 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx';
 // import App2 from './App2.jsx';
-import App3 from './App3.jsx';
+// import App3 from './App3.jsx';
+// import App4 from './App4.jsx';
+// import App5 from './App5.jsx';
+import App6 from './App6.jsx';
 
 // createRoot(document.getElementById('root')).render(<App />)
 // createRoot(document.getElementById('root-exercicio')).render(<App2 />);
-createRoot(document.getElementById('root-arrays')).render(<App3 />);
+// createRoot(document.getElementById('root-arrays')).render(<App3 />);
+// createRoot(document.getElementById('root-exercicio-2')).render(<App4 />);
+// createRoot(document.getElementById('root-eventos')).render(<App5 />);
+createRoot(document.getElementById('root-componentes')).render(<App6 />)
