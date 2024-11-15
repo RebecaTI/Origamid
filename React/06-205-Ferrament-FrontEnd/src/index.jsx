6b4b932f4eba358e7b3fb51eed5443dca61a19e4
RebecaTI <mebuffbeka@gmail.com>
2024-11-15 15:56:54 -0300

    exercicio react

---
+++ React/06-205-Ferrament-FrontEnd/src/index.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import App from './App.jsx';
+import App2 from './App2.jsx';
+
+// createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root-exercicio')).render(<App2 />);
