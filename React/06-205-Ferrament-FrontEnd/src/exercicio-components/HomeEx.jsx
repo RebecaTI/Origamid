403d501376de4d022ebec1d448a3ec641232d08f
RebecaTI <mebuffbeka@gmail.com>
2024-11-16 12:16:57 -0300

    Exercicio com componentes

---
+++ React/06-205-Ferrament-FrontEnd/src/exercicio-components/HomeEx.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TituloEx from './Tituloex'
+
+const HomeEx = () => {
+  return (
+    <section>
+      <TituloEx texto="Home" />
+      <p>Essa é a home do site</p>
+    </section>
+  )
+}
+
+export default HomeEx
--- React/06-205-Ferrament-FrontEnd/src/index.jsx
+++ React/06-205-Ferrament-FrontEnd/src/index.jsx
@@ -5,11 +5,15 @@ import { createRoot } from 'react-dom/client'
 // import App3 from './App3.jsx';
 // import App4 from './App4.jsx';
 // import App5 from './App5.jsx';
-import App6 from './App6.jsx';
+// import App6 from './App6.jsx';
+// import App7 from './App7.jsx';
+import App8Ex from './App8Ex.jsx';
 
 // createRoot(document.getElementById('root')).render(<App />)
 // createRoot(document.getElementById('root-exercicio')).render(<App2 />);
 // createRoot(document.getElementById('root-arrays')).render(<App3 />);
 // createRoot(document.getElementById('root-exercicio-2')).render(<App4 />);
 // createRoot(document.getElementById('root-eventos')).render(<App5 />);
-createRoot(document.getElementById('root-componentes')).render(<App6 />)
+// createRoot(document.getElementById('root-componentes')).render(<App6 />);
+// createRoot(document.getElementById('root-propriedades')).render(<App7 />);
+createRoot(document.getElementById('root-exercicio-3')).render(<App8Ex />);
