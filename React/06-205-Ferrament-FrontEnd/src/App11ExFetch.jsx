2441a1074601cdb094fa169d44487edfaff32d09
RebecaTI <mebuffbeka@gmail.com>
2024-11-19 18:57:59 -0300

    react exercicio

--- React/06-205-Ferrament-FrontEnd/src/App11ExFetch.jsx
+++ React/06-205-Ferrament-FrontEnd/src/App11ExFetch.jsx
@@ -3,13 +3,16 @@ import ProdutoEx from './exercicio-Fetch-3/ProdutoEx'
 
 const App11ExFetch = () => {
   const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
 
   async function handleClick(event) {
+    setCarregando(true);
     const response = await fetch(
       `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
     )
     const json = await response.json();
     setDados(json);
+    setCarregando(false);
   }
 
   return (
@@ -17,7 +20,8 @@ const App11ExFetch = () => {
       <button style={{ margin: '.5rem' }} onClick={handleClick}>Notebook</button>
       <button style={{ margin: '.5rem' }} onClick={handleClick}>Smartphone</button>
       <button style={{ margin: '.5rem' }} onClick={handleClick}>Tablet</button>
-      {dados && <ProdutoEx dados={dados} />}
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <ProdutoEx dados={dados} />}
     </div>
   )
 }
