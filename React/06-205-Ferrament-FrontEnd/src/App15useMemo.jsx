5aa0a51a3fc4764f31b835447986b17f9b00aba1
RebecaTI <mebuffbeka@gmail.com>
2024-12-21 11:02:34 -0300

    react

--- React/06-205-Ferrament-FrontEnd/src/App15useMemo.jsx
+++ React/06-205-Ferrament-FrontEnd/src/App15useMemo.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 
 const App15useMemo = () => {
-  const [contar, setContar] = React.useState(0);
+  const [contar, setContar] = React.useState(0)
   const valor = React.useMemo(() => {
-    const localItem = window.localStorage.getItem('preferido');
-    console.log('Aconteceu o memo');
-    return localItem;
-  }, []);
+    const localItem = window.localStorage.getItem('produto')
+    console.log('Aconteceu o memo')
+  }, [])
+  console.log(valor)
 
-  return (
-    <button onClick={() => setContar(contar + 1)}> {contar} </button>
-  )
+  return <button onClick={() => setContar(contar + 1)}>{contar}</button>
 }
 
 export default App15useMemo
